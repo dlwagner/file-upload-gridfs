05e135e969f326bff5ab74a08d4d7a3adbf55cd6
dlwagner <wagner.don@gmail.com>
2020-02-11 20:37:20 -0500

    add route controller code

--- controllers/issueController.js
+++ controllers/issueController.js
@@ -2,6 +2,9 @@ var Issue = require('../models/issue');
 
 var async = require('async');
 
+const { body, validationResult } = require('express-validator');
+const { sanitizeBody } = require('express-validator');
+
 exports.index = function(req, res) {
   async.parallel(
     {
@@ -20,24 +23,113 @@ exports.index = function(req, res) {
 };
 
 // Display list of all issues.
-exports.issue_list = function(req, res) {
-  res.send('NOT IMPLEMENTED: issue list');
+exports.issue_list = function(req, res, next) {
+  Issue.find({}, 'title description').exec(function(err, list_issues) {
+    if (err) {
+      return next(err);
+    }
+    //Successful, so render
+    res.render('issue_list', { title: 'Issue List', issue_list: list_issues });
+  });
 };
 
 // Display detail page for a specific issue.
 exports.issue_detail = function(req, res) {
-  res.send('NOT IMPLEMENTED: Issue detail: ' + req.params.id);
+  async.parallel(
+    {
+      issue: function(callback) {
+        Issue.findById(req.params.id).exec(callback);
+      },
+    },
+    function(err, results) {
+      if (err) {
+        return next(err);
+      }
+      if (results.issue == null) {
+        // No results.
+        var err = new Error('Issue not found');
+        err.status = 404;
+        return next(err);
+      }
+      // Successful, so render.
+      res.render('issue_detail', {
+        issue: results.issue,
+      });
+    }
+  );
 };
 
 // Display issue create form on GET.
-exports.issue_create_get = function(req, res) {
-  res.send('NOT IMPLEMENTED: issue create GET');
+exports.issue_create_get = function(req, res, next) {
+  // Get all authors and genres, which we can use for adding to our book.
+  async.parallel(
+    {
+      // We'll put stuff here later
+    },
+    function(err, results) {
+      if (err) {
+        return next(err);
+      }
+      res.render('issue-form', { title: 'Create Issue' });
+    }
+  );
 };
 
 // Handle issue create on POST.
-exports.issue_create_post = function(req, res) {
-  res.send('NOT IMPLEMENTED: Issue create POST');
-};
+exports.issue_create_post = [
+  // Validate fields.
+  body('title', 'Title must not be empty.')
+    .isLength({ min: 1 })
+    .trim(),
+  body('description', 'Description must not be empty.')
+    .isLength({ min: 1 })
+    .trim(),
+
+  // Sanitize fields (using wildcard).
+  sanitizeBody('*').escape(),
+
+  // Process request after validation and sanitization.
+  (req, res, next) => {
+    // Extract the validation errors from a request.
+    const errors = validationResult(req);
+
+    // Create an Issue object with escaped and trimmed data.
+    var issue = new Issue({
+      title: req.body.title,
+      description: req.body.description,
+    });
+
+    if (!errors.isEmpty()) {
+      // There are errors. Render form again with sanitized values/error messages.
+
+      // Show the form.
+      async.parallel(
+        {
+          // We'll put stuff here later
+        },
+        function(err, results) {
+          if (err) {
+            return next(err);
+          }
+          res.render('issue-form', {
+            title: 'Create Issue',
+            errors: errors.array(),
+          });
+        }
+      );
+      return;
+    } else {
+      // Data from form is valid. Save issue.
+      issue.save(function(err) {
+        if (err) {
+          return next(err);
+        }
+        //successful - redirect to new issue record.
+        res.redirect(issue.url);
+      });
+    }
+  },
+];
 
 // Display issue delete form on GET.
 exports.issue_delete_get = function(req, res) {
