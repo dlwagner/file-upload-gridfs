d2bb96a2118983dfb97fcc4df89eece894c9be18
dlwagner <wagner.don@gmail.com>
2020-02-18 19:08:37 -0500

    cleanup appjs

--- app.js
+++ app.js
@@ -3,18 +3,13 @@ const createError = require('http-errors');
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
-const crypto = require('crypto');
-const multer = require('multer');
-const GridFsStorage = require('multer-gridfs-storage');
-const Grid = require('gridfs-stream');
 const methodOverride = require('method-override');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
-const mongoose = require('mongoose');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-var listRouter = require('./routes/list'); // Import routes for "list" area of site
+var listRouter = require('./routes/list');
 
 dotenv.config();
 
@@ -33,31 +28,6 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-/*
-// Set up mongoose connection
-const mongoDB = process.env.DB_URL;
-
-const conn = mongoose.connect(mongoDB, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useCreateIndex: true,
-});
-
-mongoose.Promise = global.Promise;
-const db = mongoose.connection;
-
-// Create GridFS storage engine for multer
-const storage = new GridFsStorage({ db: db });
-const upload = multer({ storage });
-// console.log('db, storage: ' + db, storage);
-// console.log('upload: ' + upload);
-
-db.on(
-  'error',
-  // eslint-disable-next-line no-console
-  console.error.bind(console, 'MongoDB connection error:')
-);
-*/
 
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -81,7 +51,5 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
-
-//module.exports.upload = upload;
 
 module.exports = app;
